f3239b462bd40568df48ff510b65e2e1789f6e57
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-04-07 08:01:32 +0300

    added work

---
+++ index.js
@@ -0,0 +1,12 @@
+function run(){
+    var image = document.getElementById("background")
+    image.onload = function (){
+        var engine = new RainyDay({
+            image: this
+        });
+        engine.rain([
+            [3,2,2]
+        ], 100);
+    };image.crossOrigin = "anonymous";
+    image.src = "http://i.imgur.com/uiTqqdw.jpg";
+}
